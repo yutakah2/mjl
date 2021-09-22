d21a58cf4b9f0eef193a7fe77f256957d3245cdf
yutakah2 <youzhangnei76@gmail.com>
2021-09-23 03:18:23 +0900

    ビューの作成
    トップ、新規登録、ログイン、新規投稿

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,17 @@
 class ArticlesController < ApplicationController
 
   def index
+    query = "SELECT * FROM articles"
+    @articles = Article.find_by_sql(query)
   end
-  
+
+  def new
+    @article = Article.new
+  end
+
+  def search
+    @articles = SearchArticlesService.search(params[:keyword])
+  end
+
+  private
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ApplicationRecord
+
+  def self.search(search)
+    if search != ""
+      Article.where('text LIKE(?)', "%#{search}%")
+    else
+      Article.all
+    end
+  end
 end
