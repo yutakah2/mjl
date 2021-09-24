5346fee9afc2d86aa994ca1a79030eefaa2bf0d2
yutakah2 <youzhangnei76@gmail.com>
2021-09-24 19:37:43 +0900

    画像投稿機能②
    画像データの保存、保存した画像をトップページに表示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,20 @@ class ArticlesController < ApplicationController
   def index
     query = "SELECT * FROM articles"
     @articles = Article.find_by_sql(query)
+    @article = Article.all.includes(:user).order('created_at DESC')
   end
 
   def new
     @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def search
@@ -14,6 +24,7 @@ class ArticlesController < ApplicationController
   end
 
   private
+
   def article_params
     params.require(:article).permit(:title, :text, :image).merge(user_id: current_user.id)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_182640) do
+ActiveRecord::Schema.define(version: 2021_09_23_075034) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -36,12 +36,10 @@ ActiveRecord::Schema.define(version: 2021_09_22_182640) do
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "text", null: false
-    t.integer "user_id", null: false
-    t.integer "comment_id", null: false
-    t.integer "like_id", null: false
-    t.integer "calendar_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -58,4 +56,5 @@ ActiveRecord::Schema.define(version: 2021_09_22_182640) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "articles", "users"
 end
