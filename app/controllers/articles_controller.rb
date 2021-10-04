54acd3ad528c8b61f66ad8ce41caa7f0276d69a9
yutakah2 <youzhangnei76@gmail.com>
2021-10-04 11:32:24 +0900

    ビューの作成②
    記事詳細ページ、READEMEの編集

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,25 @@ class ArticlesController < ApplicationController
   def index
     query = "SELECT * FROM articles"
     @articles = Article.find_by_sql(query)
+    @article = Article.all
+    #render :show
   end
 
   def new
     @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def search
@@ -14,4 +29,7 @@ class ArticlesController < ApplicationController
   end
 
   private
+  def article_params
+    params.require(:article).permit(:title, :text, :image).merge(user_id: current_user.id)
+  end
 end
